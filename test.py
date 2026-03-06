b6bb25847ede746e46f29bc276c141bf5f9abae5
Piyarul7290 <piyarusekh729@gmail.com>
2026-03-06 23:32:46 +0900

    new file add

---
+++ test.py
@@ -0,0 +1,15 @@
+
+print("Hello, World")
+
+x = 10
+y = 3
+
+print(f"Sum: {x + Y}")
+print(f"Product: {x * Y}")
+
+
+fruits = ["apple", "banana", "cherry"]
+for  fruit in fruits:
+     print(f" I like {fruit}")
+
+
